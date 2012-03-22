0b5f463c031a57e799499ef957f8804f66df1e7b
Jacob Oscarson <jacob@plexical.com>
2012-03-22 20:04:13 +0100

    Crude grouping of minimum controls for recurrence

--- timely/admin.py
+++ timely/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
 from models import Timely
+
+class TimelyAdminMixin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': (
+                    ('start', 'end', 'day'),
+                    ('recurrence', 'period', 'repeats'))
+                }),
+    )
